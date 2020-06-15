ae968cfd4e83fb12e83df9b64a46ee6505907a7b
michaelerichsen <michael.erichsen@xact.dk>
2020-06-15 10:26:01 +0200

    Calling CICS COBOL program debugged and renamed

--- zcxRestService/CICS2ZCX.cbl
+++ zcxRestService/CICS2ZCX.cbl
@@ -1,5 +1,5 @@
 ﻿       IDENTIFICATION DIVISION.
-       PROGRAM-ID. CALLZCXS.
+       PROGRAM-ID. CICS2ZCX.
        AUTHOR. Michael Erichsen, Xact Consulting.
        ENVIRONMENT DIVISION.
        DATA DIVISION.
@@ -9,10 +9,10 @@
           03 HOST           PIC X(16).
           03 HOSTLENGTH     PIC S9(8) USAGE BINARY.
           03 PORTNUMBER     PIC S9(8) USAGE BINARY.
-          03 SESSTOKEN      PIC S9(8) USAGE BINARY.
+          03 SESSTOKEN      PIC X(8).
           03 PATH           PIC X(80).
           03 PATHLENGTH     PIC S9(8) USAGE BINARY.
-          03 RESPONSE       PIC X(80).
+          03 RESPONSE       PIC X(512).
           03 MAXLENGTH      PIC S9(8) USAGE BINARY.
           03 TOLENGTH       PIC S9(8) USAGE BINARY.
           03 STATUSCODEBIN  PIC S9(8) USAGE BINARY.
@@ -20,18 +20,17 @@
           03 STATUSTEXT     PIC X(80).
           03 STATUSLENGTH   PIC S9(8) USAGE BINARY.
           03 MEDIATYPE      PIC X(56).
-          03 MESSAGELENGTH  PIC S9(8) USAGE BINARY.
-          03 MESSAGEOUT     PIC X(255).
-
+          03 MSGLENGTH      PIC S9(4) USAGE BINARY.
 
        PROCEDURE DIVISION .
            INITIALIZE WS.
            MOVE '192.168.10.199' TO HOST.
            MOVE 14 TO HOSTLENGTH.
-           MOVE 8080 TO PORTNUMBER.
+           MOVE 80 TO PORTNUMBER.
            MOVE '/api/person' TO PATH.
            MOVE 11 TO PATHLENGTH.
-           MOVE 80 TO MAXLENGTH.
+           MOVE 512 TO MAXLENGTH.
+           MOVE 80 TO STATUSLENGTH.
 
            EXEC CICS WEB OPEN
                 HTTP
@@ -54,23 +53,26 @@
                 MEDIATYPE(MEDIATYPE)
                 END-EXEC.
 
-           EXEC CICS WEB CLOSE
-                SESSTOKEN(SESSTOKEN)
-                END-EXEC.
+
 
            MOVE STATUSCODEBIN TO STATUSCODE.
 
-           STRING STATUSCODE DELIMITED BY SIZE
-                  STATUSTEXT DELIMITED BY SIZE
-                  RESPONSE DELIMITED BY SIZE
-              INTO MESSAGEOUT
-           END-STRING.
+           IF STATUSCODE = 200 THEN
+              MOVE TOLENGTH TO MSGLENGTH
+              EXEC CICS SEND TEXT ERASE FREEKB
+                   FROM (RESPONSE)
+                   LENGTH(MSGLENGTH)
+                   END-EXEC
+           ELSE
+              MOVE STATUSLENGTH TO MSGLENGTH
+              EXEC CICS SEND TEXT ERASE FREEKB
+                   FROM (STATUSTEXT)
+                   LENGTH(MSGLENGTH)
+                   END-EXEC
+           END-IF.
 
-           COMPUTE MESSAGELENGTH = 6 + STATUSLENGTH + TOLENGTH.
-
-           EXEC CICS SEND TEXT
-                FROM (MESSAGEOUT)
-                LENGTH(MESSAGELENGTH)
+           EXEC CICS WEB CLOSE
+                SESSTOKEN(SESSTOKEN)
                 END-EXEC.
 
            EXEC CICS RETURN
